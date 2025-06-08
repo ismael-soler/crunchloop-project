6e95a1068bcb0e9f72b3ebcd30bd23536df7bac5
ismael-soler <ismaelsolerferreira@gmail.com>
2025-06-08 20:18:21 +0000

    Added netcat to wait for database to be running.

--- src/TodoApi/entrypoint.sh
+++ src/TodoApi/entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Ensures script will exit if a command fails
 set -e
 
-# Apply DB mgrations
-echo "Applying Database migartions.."
+# Get the database host from an environment variable
+DB_HOST=${DB_HOST:-database}
+DB_PORT=1433
+
+echo "Waiting for database at $DB_HOST:$DB_PORT..."
+
+# Using netcat to check if the port is open on the host
+while ! nc -z $DB_HOST $DB_PORT; do
+  # Sleep for a second and try again
+  sleep 1
+done
+
+echo "Database is up, applying migrations..."
 dotnet ef database update
 
-# execute the command passes to the script
-# (dotnet blabla.dll) to start de api
+# Execute the main command to start de API
 exec "$@"
